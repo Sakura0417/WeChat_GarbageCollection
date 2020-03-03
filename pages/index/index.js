a5baf27f57d358618da4fd18440e8982a6bd3f49
caoshaobing <18333964213@163.com>
2020-03-03 18:35:39 +0800

    3.3 update
    更新了图片搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    text: ''
+    text: '',
+    name: ''
   },
 
   /**
@@ -13,40 +14,26 @@ Page({
    */
   onLoad: function(options) {
     wx.removeStorageSync('input'),
-    wx.removeStorageSync('picture')
+      wx.removeStorageSync('picture')
+      this.setData({
+        name: ''
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+  //生命周期函数--监听页面初次渲染完成
+  onReady: function() {},
 
-  },
+  //生命周期函数--监听页面显示
+  onShow: function() {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
+  // 生命周期函数--监听页面隐藏
+  onHide: function() {},
 
-  },
+ 
+  // 生命周期函数--监听页面卸载
+  onUnload: function() {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  //生命周期函数--监听用户下拉动作
   onPullDownRefresh: function() {
 
   },
@@ -73,12 +60,16 @@ Page({
 
   // 拍照
   ontap: function() {
-
+    // 为了防止冲突，点击图片搜索时清楚文字搜索缓存
+    this.setData({
+      name: ' '
+    })
+    wx.removeStorageSync('input')
+    
     var that = this
     wx.showActionSheet({
       itemList: ['拍照', '从相册中选择'],
-      success(res) {
-        console.log(res.tapIndex)
+      success: function(res) {
         if (res.tapIndex == 0) { //0是拍照
           wx.chooseImage({
             count: 1,
@@ -86,44 +77,57 @@ Page({
             sourceType: ['camera'],
             success: function(res) {
               //res.tempFilePaths[0] 这个是图片
-              const tempFilePaths = res.tempFilePaths
-              console.log(tempFilePaths)
-              // 将图片路径放入缓存
-              wx.setStorage({
-                key: 'picture',
-                data: res.tempFilePaths,
+              // 将图片的本地路径进行base64编码并放入缓存
+              wx.getFileSystemManager().readFile({
+                filePath: res.tempFilePaths[0], //选择图片返回的相对路径
+                encoding: 'base64', //编码格式
+                success: function(res) { //成功的回调
+                  // console.log( res.data)
+                  wx.setStorage({
+                    key: 'picture',
+                    data: 'data:image/png;base64,'+ res.data,
+                  })
+                }
               })
-            },
-            complete: function(res) {
               wx.navigateTo({
                 url: '/pages/search_detail/search_detail',
               })
             },
+            complete: function(res) {
+            },
           })
         } else if (res.tapIndex == 1) {
-
           wx.chooseImage({
             count: 1, //选择图库中的图片
             sizeType: ['compressed'],
             sourceType: ['album'],
             success: function(res) {
               //res.tempFilePaths[0] 这个是图片
-              const tempFilePaths = res.tempFilePaths
-              console.log(tempFilePaths)
-              wx.setStorage({
-                key: 'picture',
-                data: res.tempFilePaths,
+              wx.getFileSystemManager().readFile({
+                filePath: res.tempFilePaths[0], //选择图片返回的相对路径
+                encoding: 'base64', //编码格式
+                success: function(res) { //成功的回调
+                  // console.log( res.data)
+                  wx.setStorage({
+                    key: 'picture',
+                    data: 'data:image/png;base64,' + res.data,
+                  })
+                }
               })
-            },
-            complete: function(res) {
-              // 完成后跳转到搜索详情页
               wx.navigateTo({
                 url: '/pages/search_detail/search_detail',
               })
             },
+            complete: function(res) {
+            },
           })
         }
 
+      },
+      fail: function(res) {
+        wx.navigateBack({
+          delta: 2
+        })
       }
     })
 
@@ -146,17 +150,20 @@ Page({
   },
 
   onSearch: function(event) {
-    if (wx.getStorageSync('input')==""){
+    if (wx.getStorageSync('input') == "") {
       console.log("请输入正确名称")
       wx.showToast({
         title: '请输入名字',
-        icon:'none',
-        duration:2000,
-        mask:true,
-        image:'../../images/failed.png'
+        icon: 'none',
+        duration: 2000,
+        mask: true,
+        image: '../../images/failed.png'
       })
-    }else{
+    } else {
       console.log(wx.getStorageSync('input'))
+      this.setData({
+        name: ''
+      })
       wx.navigateTo({
         url: '/pages/search_detail/search_detail',
       })
--- pages/search_detail/search_detail.js
+++ pages/search_detail/search_detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-   
+    // liststyle: false
   },
 
   /**
@@ -13,83 +13,100 @@ Page({
    */
   onLoad: function(options) {
     var that = this
-    // 页面加载时读取缓存中的信息
-    var tempFilePaths = wx.getStorageSync('picture')
-    this.setData({
-      userimage: tempFilePaths[0],
-    })
+    that.sendDatatoServer()
+
+  },
 
 
+  // 向服务器发送数据
+  sendDatatoServer: function() {
+    var that = this
+    // 将读取到的数据发送到服务器
+    // 判断用户输入的是文字还是图片
+    if (wx.getStorageSync('input')) {
+      wx.request({
+        url: 'https://recover2.market.alicloudapi.com/recover_word',
+        data: {
+          name: wx.getStorageSync('input') //读取缓存中用户输入的信息
+        },
+        method: 'get',
+        header: {
+          'content-type': 'application/json',
+          'Authorization': 'APPCODE ' + 'c977859da234401b9dbe99c27b0a14d1',
+        },
+        success(res) {
+          wx.showLoading({
+            title: '玩命搜索中',
+          })
+          console.log(res.data.data.list)
+          // 如果成功就执行处理数据
+          if (res.data.data.length == 0) {
+            console.log("找不到相关信息")
+            wx.showModal({
+              title: '提示',
+              content: '找不到相关信息，请重新搜索',
+              showCancel: false,
+              success(res) {
+                wx.navigateBack({
+                  delta: 2
+                })
+              }
+            })
+          } else {
+            that.setData({
+              garbageinfo: res.data.data.list,
+              liststyle: true
+            })
+            // that.processGarbageData(res.data.data)
+          }
+        },
+        fail: function(res) {
+          console.log(res);
+
+        },
+        complete: function(res) {
+          wx.hideLoading();
+        }
+      })
+    } else if (wx.getStorageInfoSync('picture')) {
+      wx.showLoading({
+        title: '玩命搜索中',
+      })
+      wx.request({
+        url: 'https://recover.market.alicloudapi.com/recover',
+        data: {
+          img: wx.getStorageSync('picture') //读取缓存中BASE64编码后的图片URL
+        },
+        method: 'post',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Authorization': 'APPCODE ' + 'c977859da234401b9dbe99c27b0a14d1',
+        },
+        success(res) {
+          // console.log(res.data.data)
+          that.setData({
+            garbageSimilarityData: res.data.data,
+            liststyle: false
+          })
+          // that.processGarbageImgData(res.data)
+        },
+        fail: function(res) {
+          console.log("失败")
+          console.log(res);
+        },
+        complete: function(res) {
+          // console.log("搜索完成！");
+          wx.hideLoading();
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
-    var that = this
-    // 将读取到的数据发送到服务器
-    wx.request({
-      url: 'https://recover2.market.alicloudapi.com/recover_word',
-      data: {
-        name: wx.getStorageSync('input') //读取缓存中用户输入的信息
-      },
-      method: 'get',
-      header: {
-        'content-type': 'application/json',
-        'Authorization': 'APPCODE ' + 'c977859da234401b9dbe99c27b0a14d1',
-      },
+  onReady: function() {},
 
-      success(res) {
-        // console.log(res.data)
-        console.log(res.data.data)
-
-        // 如果成功就执行处理数据
-        if (res.data.data.length==0){
-          console.log("找不到相关信息")
-          wx.showModal({
-            title: '提示',
-            content: '找不到相关信息，请重新搜索',
-            showCancel:false,
-            success(res){
-              wx.navigateBack({
-                delta: 2
-              })
-            }
-          })
-        } else {
-          that.processGarbageData(res.data.data)
-        }
-    
-
-      },
-      fail: function (res) {
-        console.log(res);
-
-      },
-      complete: function (res) {
-        console.log("搜索完成！");
-      }
-
-    })
-  },
-  // 处理服务器返回的数据
-  processGarbageData: function(arbageInfo){
-    var garbageData = [];//存放筛选过的数据
-    for (var idx in arbageInfo.list){
-      var list = arbageInfo.list[idx];
-      var name = list.name;
-      var category = list.category;
-      var temp = {
-        name:name,
-        category:category,
-      }
-      garbageData.push(temp)
-    }
-    // console.log(garbageData)
-    this.setData({
-      garbageinfo:garbageData
-    })
-  },
 
   /**
    * 生命周期函数--监听页面显示
@@ -109,13 +126,8 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
-    wx.removeStorage({
-      key: 'picture',
-      success(res) {
-        console.log(res)
-      }
-    })
     wx.removeStorageSync('input')
+    wx.removeStorageSync('picture')
   },
 
   /**
